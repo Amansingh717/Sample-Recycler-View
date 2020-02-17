5944b15b024a17c0943c8a4d95dd97a48e742052
Aman Singh <aman.singh717@gmail.com>
2020-02-17 14:34:48 +0530

    committed simple implementation for recycler view.

--- app/src/main/java/com/example/bootcamplayoutspart2/MainActivity.java
+++ app/src/main/java/com/example/bootcamplayoutspart2/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.bootcamplayoutspart2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DataSource mDataSource;
+    private RecyclerView mRecyclerView;
+    private MyRecyclerViewAdapter myRecyclerViewAdapter;
+    private LinearLayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialisation
+        mDataSource = new DataSource();
+        mRecyclerView = findViewById(R.id.recycler_view);
+        myRecyclerViewAdapter = new MyRecyclerViewAdapter();
+        layoutManager = new LinearLayoutManager(this);
+
+        //fetching data
+        myRecyclerViewAdapter.addData(mDataSource.getData());
+
+        //setting recycler-view
+        mRecyclerView.setAdapter(myRecyclerViewAdapter);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(false);
     }
 }
---
+++ app/src/main/java/com/example/bootcamplayoutspart2/MyRecyclerViewAdapter.java
@@ -0,0 +1,46 @@
+package com.example.bootcamplayoutspart2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.MyRecyclerViewHolder> {
+
+    private ArrayList<Double> arrayList = new ArrayList<>();
+
+    void addData(ArrayList<Double> arrayList) {
+        this.arrayList.addAll(arrayList);
+    }
+
+    @NonNull
+    @Override
+    public MyRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_row_item, parent, false);
+        return new MyRecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyRecyclerViewHolder holder, int position) {
+        holder.textView.setText(String.valueOf(arrayList.get(position)));
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    class MyRecyclerViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        MyRecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.row_item_text);
+        }
+    }
+}
